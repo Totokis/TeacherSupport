081038aac98f4781ff68f2ddcbe42e966368f05e
Paweł <pawelj_janusz@wp.pl>
2020-12-02 11:04:05 +0100

    Base implementation of view model for Groups

--- src/main/java/com/example/project_nr_one/GroupFragment.kt
+++ src/main/java/com/example/project_nr_one/GroupFragment.kt
@@ -1,13 +1,20 @@
 package com.example.project_nr_one
 
-import androidx.lifecycle.ViewModelProviders
 import android.os.Bundle
 import androidx.fragment.app.Fragment
 import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
+import androidx.fragment.app.viewModels
+import androidx.lifecycle.Observer
 
 class GroupFragment : Fragment() {
 
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        val model: GroupViewModel by viewModels()
+        model.getGroups().observe(this, Observer<List<Group>> { groups->
 
+        })
+    }
 }
