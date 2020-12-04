9c42d07171fb9a13ba90d859a796858c2e36a66c
Paweł <pawelj_janusz@wp.pl>
2020-12-04 15:34:08 +0100

    Logic for basic navigation

--- app/src/main/java/com/example/testproject/View/GroupFragment.kt
+++ app/src/main/java/com/example/testproject/View/GroupFragment.kt
@@ -1,7 +1,20 @@
 package com.example.testproject.View
 
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import com.example.testproject.R
 
 class GroupFragment : Fragment(){
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        val view: View = inflater.inflate(R.layout.group_fragment, container, false)
+        return view
+    }
 
 }
--- app/src/main/java/com/example/testproject/View/StartFragment.kt
+++ app/src/main/java/com/example/testproject/View/StartFragment.kt
@@ -5,15 +5,23 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.findNavController
 import com.example.testproject.R
+import kotlinx.android.synthetic.main.start_fragment.view.*
 
 class StartFragment : Fragment()
 {
+
     override fun onCreateView(
         inflater: LayoutInflater,
         container: ViewGroup?,
         savedInstanceState: Bundle?
     ): View? {
-        return inflater.inflate(R.layout.start_fragment, container, false);
+        val view: View = inflater.inflate(R.layout.start_fragment, container, false)
+        view.button.setOnClickListener{
+            view.findNavController().navigate(R.id.action_startFragment_to_groupFragment)
+
+        }
+        return view;
     }
 }
