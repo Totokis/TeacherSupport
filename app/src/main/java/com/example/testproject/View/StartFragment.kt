9e2381d25bdb4851e27445a2859842541589016c
Paweł <pawelj_janusz@wp.pl>
2020-12-04 14:16:11 +0100

    View class for start fragment

---
+++ app/src/main/java/com/example/testproject/View/StartFragment.kt
@@ -0,0 +1,19 @@
+package com.example.testproject.View
+
+import android.os.Bundle
+import android.view.LayoutInflater
+import android.view.View
+import android.view.ViewGroup
+import androidx.fragment.app.Fragment
+import com.example.testproject.R
+
+class StartFragment : Fragment()
+{
+    override fun onCreateView(
+        inflater: LayoutInflater,
+        container: ViewGroup?,
+        savedInstanceState: Bundle?
+    ): View? {
+        return inflater.inflate(R.layout.start_fragment, container, false);
+    }
+}
