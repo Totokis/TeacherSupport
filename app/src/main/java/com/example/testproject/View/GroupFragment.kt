6959f94d1439d6ba547fc2065ec9e08a4babdc2a
Paweł <pawelj_janusz@wp.pl>
2020-12-15 16:20:19 +0100

    Group view and viewmodel logic

--- app/src/main/java/com/example/testproject/View/GroupFragment.kt
+++ app/src/main/java/com/example/testproject/View/GroupFragment.kt
@@ -5,7 +5,9 @@ import android.view.LayoutInflater
 import android.view.View
 import android.view.ViewGroup
 import androidx.fragment.app.Fragment
+import androidx.navigation.fragment.findNavController
 import com.example.testproject.R
+import kotlinx.android.synthetic.main.group_fragment.view.*
 
 class GroupFragment : Fragment(){
     override fun onCreateView(
@@ -14,6 +16,9 @@ class GroupFragment : Fragment(){
         savedInstanceState: Bundle?
     ): View? {
         val view: View = inflater.inflate(R.layout.group_fragment, container, false)
+        view.floatingActionButton.setOnClickListener{
+            findNavController().navigate(R.id.action_groupFragment_to_addGroup)
+        }
         return view
     }
 }
--- app/src/main/java/com/example/testproject/ViewModel/GroupViewModel.kt
+++ app/src/main/java/com/example/testproject/ViewModel/GroupViewModel.kt
@@ -1,12 +1,28 @@
 package com.example.testproject.ViewModel
 
-import androidx.lifecycle.MutableLiveData
-import androidx.lifecycle.ViewModel
+import android.app.Application
+import androidx.lifecycle.*
+import com.example.testproject.Model.data.Group
+import com.example.testproject.Model.data.GroupDatabase
+import com.example.testproject.Model.data.GroupRepository
+import kotlinx.coroutines.Dispatchers
+import kotlinx.coroutines.launch
 
-class GroupViewModel : ViewModel(){
-//    private val users: MutableLiveData<List<Group>> by lazy {
-//        MutableLiveData().also{
-//            loadGroups()
-//        }
-//    }
+class GroupViewModel(application: Application) : AndroidViewModel(application){
+
+    private val readAllData: LiveData<List<Group>>
+    private val repository: GroupRepository
+
+    init{
+        val groupDao = GroupDatabase.getDatabase(application).groupDao()
+        repository = GroupRepository(groupDao)
+        readAllData = repository.readAllData
+
+    }
+
+    fun addGroup(group: Group){
+        viewModelScope.launch(Dispatchers.IO){
+            repository.addGroup(group)
+        }
+    }
 }
